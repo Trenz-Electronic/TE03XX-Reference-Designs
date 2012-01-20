f76d779ac6a56e73c4dc083e5890e4e037629ad1
Oleksandr Kienko <a.kienko@gmail.com>
2012-01-20 11:02:25 +0200

    Added release script making zip archives

---
+++ release.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Projects to release
+REPOSITORY=TE03XX-Reference-Designs
+# Numbers of projects to release (list like '1 2 3 4')
+PROJECT_LIST='1 2 3'
+# Project 1 description
+# List of folders requred to release (First will be used as name)
+NAME1='reference-TE0300 TE-EDK-IP'
+# Tool used to build
+TOOL1='EDK-13.2'
+# Project Version
+VER1='1.0'
+
+# Project 2 description
+NAME2='reference-TE0320 TE-EDK-IP'
+TOOL2='EDK-13.2'
+VER2='1.0'
+
+# Project 3 description
+NAME3='blinkin-TE0300'
+TOOL3='ISE-13.2'
+VER3='1.0'
+
+############# Do not edit below this line !!!
+SUBVER=`git rev-parse --short HEAD`
+
+if [ -z "$PROJECT_LIST" ]; then exit ; fi
+cd ..
+for l in $PROJECT_LIST
+do
+	namelist=NAME$l
+	tool=TOOL$l
+	ver=VER$l
+	name=${!namelist}
+	set $name
+	for proj in ${!namelist}
+	do
+		zip -rq $1-${!tool}-v${!ver}.${SUBVER}.zip ${REPOSITORY}/${proj}
+	done
+done
+cd ${REPOSITORY}
+
