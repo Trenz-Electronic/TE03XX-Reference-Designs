5e018412728af12fbeaf8173b23ef8f6e3aeee31
Oleksandr Kienko <a.kienko@gmail.com>
2012-02-23 19:15:23 +0200

    blinkin-TE0300 replaced by new version for 1200 and 1600 chips

---
+++ blinkin-TE0300/build_all.bat
@@ -0,0 +1,50 @@
+rem Configure environment
+set XILINX=C:\Xilinx\13.2\ISE_DS\ISE
+set XILINX_DSP=%XILINX%
+set PATH=%XILINX%\bin\nt;%XILINX%\lib\nt;%PATH%
+
+rem Run 1200 version build
+xtclsh blinkin-TE0300-1200.tcl rebuild_project
+promgen -w -u 0 blinkin.bit -o TE0300-1200.mcs
+promgen -w -p bin -u 0 blinkin.bit -o fpga.bin
+copy blinkin.bit TE0300-1200.bit
+zip -q TE0300-1200.zip fpga.bin Bootload.ini usb.bin
+move TE0300-1200.zip TE0300-1200.fwu
+
+rem Clean
+@del /F /q fpga.bin fpga.cfi fpga.prm
+@del /F /q TE0300-1200.cfi TE0300-1200.prm
+@del /F /q bitgen.xmsgs map.xmsgs ngdbuild.xmsgs par.xmsgs trce.xmsgs xst.xmsgs
+@del /F /q blinkin.bgn blinkin.bit blinkin.bld blinkin.cmd_log blinkin.drc blinkin.lso
+@del /F /q blinkin.ncd blinkin.ngc blinkin.ngd blinkin.ngr blinkin.pad blinkin.par
+@del /F /q blinkin.pcf blinkin.prj blinkin.ptwx blinkin.stx blinkin.syr blinkin.twr
+@del /F /q blinkin.twx blinkin.unroutes blinkin.ut blinkin.xpi blinkin.xst
+@del /F /q blinkin_map.map blinkin_map.mrp blinkin_map.ncd blinkin_map.ngm
+@del /F /q blinkin_map.xrpt blinkin_ngdbuild.xrpt blinkin_pad.csv blinkin_pad.txt
+@del /F /q blinkin_par.xrpt blinkin_summary.html blinkin_summary.xml blinkin_usage.xml
+@del /F /q blinkin_vhdl.prj blinkin_xst.xrpt webtalk.log webtalk_pn.xml
+@rmdir /S /Q xst _ngo xlnx_auto_0_xdb
+@del /F /q blinkin_summary.html blinkin_guide.ncd
+
+rem Run 1200 version build
+xtclsh blinkin-TE0300-1600.tcl rebuild_project
+promgen -w -u 0 blinkin.bit -o TE0300-1600.mcs
+promgen -w -p bin -u 0 blinkin.bit -o fpga.bin
+copy blinkin.bit TE0300-1600.bit
+zip -q TE0300-1600.zip fpga.bin Bootload.ini usb.bin
+move TE0300-1600.zip TE0300-1600.fwu
+
+rem Clean
+@del /F /q fpga.bin fpga.cfi fpga.prm
+@del /F /q TE0300-1600.cfi TE0300-1600.prm
+@del /F /q bitgen.xmsgs map.xmsgs ngdbuild.xmsgs par.xmsgs trce.xmsgs xst.xmsgs
+@del /F /q blinkin.bgn blinkin.bit blinkin.bld blinkin.cmd_log blinkin.drc blinkin.lso
+@del /F /q blinkin.ncd blinkin.ngc blinkin.ngd blinkin.ngr blinkin.pad blinkin.par
+@del /F /q blinkin.pcf blinkin.prj blinkin.ptwx blinkin.stx blinkin.syr blinkin.twr
+@del /F /q blinkin.twx blinkin.unroutes blinkin.ut blinkin.xpi blinkin.xst
+@del /F /q blinkin_map.map blinkin_map.mrp blinkin_map.ncd blinkin_map.ngm
+@del /F /q blinkin_map.xrpt blinkin_ngdbuild.xrpt blinkin_pad.csv blinkin_pad.txt
+@del /F /q blinkin_par.xrpt blinkin_summary.html blinkin_summary.xml blinkin_usage.xml
+@del /F /q blinkin_vhdl.prj blinkin_xst.xrpt webtalk.log webtalk_pn.xml
+@rmdir /S /Q xst _ngo xlnx_auto_0_xdb
+@del /F /q blinkin_summary.html blinkin_guide.ncd
---
+++ blinkin-TE0300/src/blinkin.vhd
@@ -0,0 +1,75 @@
+--------------------------------------------------------------------------------
+-- Copyright (C) 2012 Trenz Electronic
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a 
+-- copy of this software and associated documentation files (the "Software"), 
+-- to deal in the Software without restriction, including without limitation 
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+-- and/or sell copies of the Software, and to permit persons to whom the 
+-- Software is furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included 
+-- in all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+-- OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+-- IN THE SOFTWARE.
+--------------------------------------------------------------------------------
+-- Company: Trenz Electronic
+-- Engineer: Oleksandr Kiyenko
+-- Project Name: blinkin
+-- Target Devices: Spartan 3E XC3S500E XC3S1600E
+-- Tool versions: 13.2
+-- Description: Simple LED blinker
+-- Dependencies: No
+--------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity blinkin is
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+           led : out  STD_LOGIC);
+end blinkin;
+
+architecture Behavioral of blinkin is
+
+	constant divider : integer := 50000000;
+	signal counter : integer := 0;
+	signal led_i : std_logic := '0';
+	signal lvds_clk : std_logic := '0';
+	signal int_clk : std_logic := '0';
+	
+begin
+
+	led <= led_i;
+	int_clk <= clk;      --phy clock  125MHz
+	
+  process (rst, int_clk)
+	begin
+		if rst='1' then
+			led_i <= '0';
+			counter <= 0;
+    elsif rising_edge(int_clk) then
+	    if counter = divider-1 then
+				counter <= 0;
+			  led_i <= not led_i;
+			else
+				counter <= counter + 1;
+	    end if;
+		end if;
+	end process;
+
+end Behavioral;
+
