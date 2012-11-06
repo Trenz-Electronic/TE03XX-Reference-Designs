20b59e53dac8e76fc2c37c948c072b08e36b388d
Oleksandr Kiyenko <a.kienko@gmail.com>
2012-11-06 12:06:15 +0100

    Added utility scripts

---
+++ reference-TE0300/make_bin.bat
@@ -0,0 +1,17 @@
+rem Configure environment
+call C:\Xilinx\13.2\ISE_DS\settings64.bat
+
+@rem Configure environment
+set XILINX=C:\Xilinx\13.2\ISE_DS\ISE
+set XILINX_DSP=%XILINX%
+set PATH=%XILINX%\bin\nt;%XILINX%\lib\nt;%PATH%
+
+@rem Generate FWU for 1200
+@copy implementation\download.bit fpga.bit
+@impact -batch etc\bit2bin.cmd
+
+@rem Remove logs
+@del fpga.sig fpga.prm fpga.cfi fpga.bit
+
+@rem Remove files
+@del _impactbatch.log
